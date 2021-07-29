b9f927a495baf55968f883395523d576c0e8c55d
agusannacondia <agustinannacondia@gmail.com>
2021-07-29 17:24:46 -0300

    Desafio: Centro de Mensajes

---
+++ Desafio: Centro de Mensajes/app.test.js
@@ -0,0 +1,34 @@
+import supertest from "supertest";
+import app from "./app.js";
+
+describe("ROUTER /products", () => {
+    describe("GET /list/:id", () => {
+        describe("Get all available products", () => {
+            test("Should respond with a json object containing an array of products", async () => {
+                const response = await request(app).get("/list");
+                expect(typeof response).toBe(JSON);
+            });
+            test("Should respond with a 200 status code", async () => {
+                const response = await request(app).get("/list");
+                expect(response.statusCode()).toBe(200);
+            });
+            test("Should specify json in the content type header", () => {
+                const response = await request(app).get("/list");
+                expect(response.headers["content-type"]).toEqual(
+                    expect.stringContaining("json")
+                );
+            });
+        });
+        describe("Get an available product with the id", () => {});
+        describe("Get an unavailable product with the id", () => {});
+    });
+    describe("POST /add", () => {});
+    describe("PUT /update/:id", () => {});
+    describe("DELETE /delete/:id", () => {});
+});
+
+describe("ROUTER /cart", () => {
+    describe("GET /list/:id", () => {});
+    describe("POST /add/:id", () => {});
+    describe("DELETE /delete/:id", () => {});
+});
